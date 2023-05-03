f1da1c35480c0cfa8c2c65f2986e9248c8160607
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-03 22:49:33 +0100

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('clients.index');
+    return redirect()->route('/');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::post('/clients/register', [ClientController::class,'store'])->name('store');
-Route::get('/clients', [ClientController::class,'index']);
+Route::get('/clients/create', [ClientController::class,'create'])->name('create');
+Route::get('/clients', [ClientController::class,'index'])->name('/');
 Route::get('/clients/show/{id}', [ClientController::class,'show'])->name('show');
