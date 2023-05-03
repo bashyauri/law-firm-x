af12f80174d4bf3735a61a160691a06673e0dcc1
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-03 22:48:53 +0100

    delete ClientController.js file

--- resources/js/controllers/ClientController.js
+++
@@ -1,7 +0,0 @@
-export default class ClientController {
-    static async index() {
-        const response = await fetch("/api/clients");
-        const clients = await response.json();
-        return clients;
-    }
-}
