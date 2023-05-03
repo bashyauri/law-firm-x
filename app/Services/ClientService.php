5e63c6789027b0a4f8af44af0a18223ef7deb7f7
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-03 18:48:23 +0100

    update client service

---
+++ app/Services/ClientService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Client;
+
+/**
+ * Class ClientService.
+ */
+class ClientService
+{
+    public function createClient(array $validatedData) : Object
+    {
+        $client = new Client();
+        $client->fill($validatedData);
+
+        if ($validatedData['profile_image']) {
+            $path = $validatedData['profile_image']->store('public/profile_images');
+            $client->profile_image = basename($path);
+        }
+
+       $client->save();
+       return $client;
+    }
+}
