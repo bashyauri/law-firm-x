5e4d32c840f2a97406de9cc9448e42ff0ad1d944
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-03 12:45:17 +0100

    create notification

---
+++ app/Notifications/ClientImageNotification.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class ClientImageNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+        ->subject('Reminder: Profile Image Required')
+        ->line('Dear ' . $notifiable->last_name . ',')
+        ->line('We noticed that your profile image is missing.')
+        ->line('Please drop your passport photograph with Law Firm X as soon as possible.')
+        ->line('Thank you for your cooperation.')
+        ->salutation('Best regards,')
+        ->from('noreply@firm-x.com', 'Firm-X');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
