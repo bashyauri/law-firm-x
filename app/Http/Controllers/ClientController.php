9a0cf2432c207557bc202e3b7019f78958c9c5b3
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-04 13:35:53 +0100

    use exam.md

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -65,27 +65,5 @@ class ClientController extends Controller
         return view('clients.show')->with(['client'=>$client,'date_profiled' => $date_profiled]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
